83a4246cad13b41c3c2db22c115cf8a53b99304a
santiagoCoy2025 <santiagoocoy2025@gmail.com>
2025-11-10 09:02:10 -0500

    NUEVOS CAMBIOS

--- PRoyecto django/Proyecto-html/Gesicom/models.py
+++ PRoyecto django/Proyecto-html/Gesicom/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=60)
+    telefono = models.CharField(max_length=20, null=True, blank=True)
+    contraseña= models.CharField(max_length=8)
+
+class Roles(models.Model):
+    Rol = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    Persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.Rol
+    
 
 
